3e24edd05118d2df91d7f6677df0e6ed3fdc7c29
Shane Czenkus <sczenkus1993@gmail.com>
2017-11-30 21:31:08 -0500

    input concat.

--- homeControl.js
+++ homeControl.js
@@ -1,26 +1,33 @@
 var app =angular.module('spotifyPlaylist');
-app.controller('homeControl', function($scope, $timeout, spotifyFactory){
+app.controller('homeControl', function($scope, $timeout, $location, spotifyFactory){
 
- 
-  spotifyFactory.getTracks(query).then(function(data){
-    console.log(data);
-    $timeout($scope.tracks = data);
-  });
+  $scope.makePlaylist = function() {
+    var search = '';
+    if ($scope.input1.length) {
+      input = $scope.input1;
+      input = input.toLowerCase();
+      input = input.replace(/\s+/g, '');
+      input = input + '+';
+      search = search + input;
+      console.log(search);
+    }
+  }
 
-
+//   spotifyFactory.getTracks(query).then(function(data){
+//     console.log(data);
+//     $timeout($scope.tracks = data);
+//   });
+//
+//
 });
-// app.controller('homeControl', function($scope, $location){
-// 	$scope.makePlaylist = function() {
-// 		console.log($scope.input1);
-// 		console.log($scope.input2);
-// 		console.log($scope.input3);
-// 		//TODO: 
-// 		//Clean inputs, then call function in factory to search Spotify using the cleaned inputs
 
-// 		//The following will call the config and change the view to the playlist page
-// 		// $location.path('/playlist');
 
-// 		//function to call factory:
-// 		//SpotifyFactory.getTracks()
-// 	}
-// });
+
+//TO DO:
+//Clean inputs, then call function in factory to search Spotify using the cleaned inputs
+
+//The following will call the config and change the view to the playlist page
+// $location.path('/playlist');
+
+//function to call factory:
+//SpotifyFactory.getTracks()
