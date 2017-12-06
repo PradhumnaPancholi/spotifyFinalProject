2523dc6929ed387d7d48ad347e06ca0028c83a83
Blair Steen <blairsteen@gmail.com>
2017-12-05 21:09:24 -0500

    added random inputs

--- homeControl.js
+++ homeControl.js
@@ -1,8 +1,17 @@
 var app =angular.module('spotifyPlaylist');
 
+var items = ['food', 'color', 'number', 'city'];
+
+
 app.controller('homeControl', function($scope, $rootScope, $location, spotifyFactory){
   $rootScope.mainBackgroundImage = 'https://static1.squarespace.com/static/55d62be6e4b0be109fdab4b5/55d74672e4b06c5ffbc15cc4/55d746aae4b0c9560c4bd747/1440171692692/_T0U0829_20m_RGB.jpg?format=2500w';
   spotifyFactory.savedTracks= null
+  $scope.placeholder = items[Math.floor(Math.random()*items.length)];
+  var placeholderList = items.filter(function(item){
+    return item !== $scope.placeholder;
+  });
+  $scope.placeholder2 = placeholderList[Math.floor(Math.random()*placeholderList.length)];
+
   $scope.makePlaylist = function() {
     //pushes all of the user inputs into an empty array 
     // var inputValues = [];
