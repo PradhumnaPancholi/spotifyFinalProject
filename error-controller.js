ea2cff59c28021dbf150d0a06ed0d0dd08f7e105
timmattimore <Timothy.mattimore@gmail.com>
2017-12-06 19:49:53 -0500

    error page fix

--- error-controller.js
+++ error-controller.js
@@ -1,6 +1,5 @@
 var app =angular.module('spotifyPlaylist');
-app.controller('errorControl', function($scope, $rootScope, $http, $sce, spotifyPlaylist) { 
-
+app.controller('errorControl', function($scope, $rootScope) { 
         $rootScope.mainBackgroundImage = 'http://www.electronicbeats.net/app/uploads/2016/06/rickastley.jpg';
         
     });
